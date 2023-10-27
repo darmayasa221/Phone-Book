3b35684207e3fb68d20bed371ac95bd068a384e2
darmayasa221 <darmayasa221@gmail.com>
2023-10-27 12:50:32 +0800

    refactor : remove code not needed

--- src/store/Contact/contactContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { IContactContext } from "../../types/store/ContactContextProvider";
-
-const ContactContext = createContext<IContactContext>({} as IContactContext);
-
-export default ContactContext;
