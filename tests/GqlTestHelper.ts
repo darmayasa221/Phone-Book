ed762f8b36cce2f63849d4f2f093fe0eef9dd1b6
darmayasa221 <darmayasa221@gmail.com>
2023-10-29 02:17:09 +0800

    refactor: refactor gql test helper

--- tests/GqlTestHelper.ts
+++ tests/GqlTestHelper.ts
@@ -1,3 +1,4 @@
+import { QueryOptions } from "@apollo/client";
 import {
   Contact_Select_Column,
   Contact_Set_Input,
@@ -14,9 +15,18 @@ import {
   UPDATE_CONTACT,
   UPDATE_NUMBER,
 } from "../src/dataProvider/contact";
-
+type mocks = {
+  request: QueryOptions;
+  result?: { data: any };
+  error?: Error;
+};
 const GqlTestHelper = {
-  async getContacts(limit: number, offset: number, search: string) {
+  async getContacts(
+    limit: number,
+    offset: number,
+    search: string,
+    error?: Error,
+  ): Promise<mocks> {
     const { data } = await apolloClient.query({
       query: GET_CONTACT,
       variables: {
@@ -31,22 +41,49 @@ const GqlTestHelper = {
         order_by: [{ created_at: Order_By.Desc }],
       },
     });
-    return data;
+    return {
+      request: {
+        query: GET_CONTACT,
+        variables: {
+          limit,
+          offset,
+          where: {
+            first_name: {
+              _like: Boolean(search) ? `%${search}%` : "%%",
+            },
+          },
+          distinct_on: [Contact_Select_Column.CreatedAt],
+          order_by: [{ created_at: Order_By.Desc }],
+        },
+      },
+      result: { data: !error ? data : undefined },
+      error,
+    };
   },
-  async getContact(id: number) {
+  async getContact(id: number, error?: Error): Promise<mocks> {
     const { data } = await apolloClient.query({
       query: GET_CONTACT_BY_ID,
       variables: {
         id,
       },
     });
-    return data;
+    return {
+      request: {
+        query: GET_CONTACT_BY_ID,
+        variables: {
+          id,
+        },
+      },
+      result: { data: !error ? data : undefined },
+      error,
+    };
   },
   async createContact(
     first_name: string,
     last_name: string,
     phones: Phone_Insert_Input,
-  ) {
+    error?: Error,
+  ): Promise<mocks> {
     const { data } = await apolloClient.query({
       query: CREATE_CONTACT,
       variables: {
@@ -55,9 +92,24 @@ const GqlTestHelper = {
         phones,
       },
     });
-    return data;
+    return {
+      request: {
+        query: CREATE_CONTACT,
+        variables: {
+          first_name,
+          last_name,
+          phones,
+        },
+      },
+      result: { data: !error ? data : undefined },
+      error,
+    };
   },
-  async updateContact(id: number, contact: Contact_Set_Input) {
+  async updateContact(
+    id: number,
+    contact: Contact_Set_Input,
+    error?: Error,
+  ): Promise<mocks> {
     const { data } = await apolloClient.query({
       query: UPDATE_CONTACT,
       variables: {
@@ -65,9 +117,23 @@ const GqlTestHelper = {
         _set: contact,
       },
     });
-    return data;
+    return {
+      request: {
+        query: UPDATE_CONTACT,
+        variables: {
+          id,
+          _set: contact,
+        },
+      },
+      result: { data: !error ? data : undefined },
+      error,
+    };
   },
-  async updateNumber(pervContact: Phone_Pk_Columns_Input, newNumber: string) {
+  async updateNumber(
+    pervContact: Phone_Pk_Columns_Input,
+    newNumber: string,
+    error?: Error,
+  ): Promise<mocks> {
     const { data } = await apolloClient.query({
       query: UPDATE_NUMBER,
       variables: {
@@ -75,16 +141,35 @@ const GqlTestHelper = {
         new_phone_number: newNumber,
       },
     });
-    return data;
+    return {
+      request: {
+        query: UPDATE_NUMBER,
+        variables: {
+          pk_columns: pervContact,
+          new_phone_number: newNumber,
+        },
+      },
+      result: { data: !error ? data : undefined },
+      error,
+    };
   },
-  async deleteContact(id: number) {
+  async deleteContact(id: number, error?: Error): Promise<mocks> {
     const { data } = await apolloClient.query({
       query: DELETE_CONTACT,
       variables: {
         id,
       },
     });
-    return data;
+    return {
+      request: {
+        query: DELETE_CONTACT,
+        variables: {
+          id,
+        },
+      },
+      result: { data: !error ? data : undefined },
+      error,
+    };
   },
 };
 
