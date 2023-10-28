9e7094f51308101fa3356e6bf9edea96564f9ffd
darmayasa221 <darmayasa221@gmail.com>
2023-10-29 02:18:25 +0800

    testing: testing contacts component

--- src/_test/App.test.tsx
+++ src/_test/App.test.tsx
@@ -1,6 +1,5 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import "@testing-library/jest-dom";
 import { RouterProvider } from "react-router-dom";
 import routes from "../routes/routes";
 describe("App", () => {
